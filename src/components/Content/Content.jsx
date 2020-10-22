196e856aa133a820e71c3cc83c038733bfe9f09c
Анастасия <eugengamilov@gmail.com>
2020-10-22 19:38:55 +0300

    planning_page_wrapper

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Planning from '../../pages/Planning';
 import styles from './Content.module.css';
 
 function Content() {
-  return <div className={styles.container}></div>;
+  return <div className={styles.container}>
+   <Planning>
+     
+   </Planning>
+  </div>;
 }
 
 export default Content;
