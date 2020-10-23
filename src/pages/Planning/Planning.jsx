3a5da4c0be33ac41ef11934461013b439f7aa9d4
Анастасия <eugengamilov@gmail.com>
2020-10-23 18:10:24 +0300

    fixed_background

--- src/pages/Planning/Planning.jsx
+++ src/pages/Planning/Planning.jsx
@@ -5,14 +5,12 @@ export default class Planning extends Component {
   render() {
     const [PlanForm, Prognosis] = this.props.children;
     return (
-      <div className={styles.wrapper}>
-      <div className={styles.planForm}>
-          {PlanForm}
+      <div className={styles.planningPage}>
+        <div className={styles.wrapper}>
+          <div className={styles.planForm}>{PlanForm}</div>
+          <div className={styles.prognosis}>{Prognosis}</div>
+        </div>
       </div>
-      <div className={styles.prognosis}>
-    {Prognosis}
-          </div>
-    </div>
     );
   }
 }
