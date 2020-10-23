d7876cdbdc44feec9583e924e34949b2de7edc85
Анастасия <eugengamilov@gmail.com>
2020-10-23 20:30:30 +0300

    plan_form_add

--- src/pages/Planning/Planning.jsx
+++ src/pages/Planning/Planning.jsx
@@ -3,12 +3,12 @@ import styles from './Planning.module.css';
 
 export default class Planning extends Component {
   render() {
-    const [PlanForm, Prognosis] = this.props.children;
+    const [PlanForm, PrognosisBuy] = this.props.children;
     return (
       <div className={styles.planningPage}>
         <div className={styles.wrapper}>
           <div className={styles.planForm}>{PlanForm}</div>
-          <div className={styles.prognosis}>{Prognosis}</div>
+          <div className={styles.prognosis}>{PrognosisBuy}</div>
         </div>
       </div>
     );
