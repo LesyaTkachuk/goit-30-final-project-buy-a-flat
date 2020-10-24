67653807761c29173f7ef11a39ddc3977e3d27fc
Zhydovlenko <you@example.com>
2020-10-24 18:46:22 +0300

    combined expense components

--- src/components/ExpenseForm/ExpenseForm.jsx
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -2,12 +2,36 @@ import React, { Component } from 'react';
 import styles from './ExpenseForm.module.css';
 
 class ExpensesForm extends Component {
+  state = {
+    account: '',
+    expenseName: '',
+    category: '',
+    amount: '',
+  };
+
+  handleAccount = e => {
+    this.setState({ account: e.target.value });
+  };
+
+  handleExpenseName = e => {
+    this.setState({ expenseName: e.target.value });
+  };
+
+  handleCategory = e => {
+    this.setState({ category: e.target.value });
+  };
+
+  handleAmount = e => {
+    this.setState({ amount: e.target.value });
+  };
+
   render() {
+    const { account, expenseName, category, amount } = this.state;
     return (
       <div className={styles.formContainer}>
         <form className={styles.form} action="">
           <div className={styles.formItem}>
-            <label className={styles.formLabel} for="account">
+            <label className={styles.formLabel} htmlFor="account">
               {/* Со счета  */}
               Сумма на счету
             </label>
@@ -16,30 +40,38 @@ class ExpensesForm extends Component {
               type="text"
               name="account"
               id="account"
-              value="100.00"
+              value={account}
+              onChange={this.handleAccount}
               readOnly
             />
-            {/* <label className={styles.formLabel} for="account">
+            {/* <label className={styles.formLabel} htmlFor="account">
               Остаток на счете:
             </label> */}
           </div>
           <div className={styles.formItem}>
-            <label className={styles.formLabel} for="cost-item">
+            <label className={styles.formLabel} htmlFor="expense-name">
               Название статьи
             </label>
             <input
               className={styles.formInput}
               type="text"
-              name="cost-item"
-              id="cost-item"
-              value="Билеты на концерт"
+              name="expense-name"
+              id="expense-name"
+              value={expenseName}
+              onChange={this.handleExpenseName}
             />
           </div>
           <div className={styles.formItem}>
-            <label className={styles.formLabel} for="category">
+            <label className={styles.formLabel} htmlFor="category">
               На категорию
             </label>
-            <select className={styles.formInput} name="category" id="category">
+            <select
+              className={styles.formInput}
+              name="category"
+              id="category"
+              value={category}
+              onChange={this.handleCategory}
+            >
               <option value="entertainment">Развлечения</option>
               <option value="health">Здоровье</option>
               <option value="products">Продукты</option>
@@ -49,14 +81,16 @@ class ExpensesForm extends Component {
             </select>
           </div>
           <div className={styles.formItem}>
-            <label className={styles.formLabel} for="sum">
+            <label className={styles.formLabel} htmlFor="amount">
               Сумма
             </label>
             <input
               className={styles.formInput}
-              type="text"
+              type="number"
               name="amount"
               id="amount"
+              value={amount}
+              onChange={this.handleAmount}
               placeholder="00.00"
             />
           </div>
