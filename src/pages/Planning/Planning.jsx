2bd38b25e3c41ce1c97291c40b84986027800bee
Анастасия <eugengamilov@gmail.com>
2020-10-24 12:07:15 +0300

    planning_page_working

--- src/pages/Planning/Planning.jsx
+++ src/pages/Planning/Planning.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import styles from './Planning.module.css';
+import PrognosisBuy from '../../components/PrognosisBuy';
+import PlanForm from '../../components/PlanForm';
 
 export default class Planning extends Component {
   render() {
-    const [PlanForm, Prognosis] = this.props.children;
     return (
       <div className={styles.planningPage}>
         <div className={styles.wrapper}>
-          <div className={styles.planForm}>{PlanForm}</div>
-          <div className={styles.prognosis}>{Prognosis}</div>
+          <div className={styles.planForm}>
+            <PlanForm />
+          </div>
+          <div className={styles.prognosis}>
+            <PrognosisBuy />
+          </div>
         </div>
       </div>
     );
