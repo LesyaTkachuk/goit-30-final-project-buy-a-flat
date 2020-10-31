5ba9134b6474a517b1d5d845f0d8f70f89e9809f
Aleksandra Tkachuk <lesyalesya9876@gmail.com>
2020-10-31 22:54:46 +0200

    added googleAuth operation

--- src/redux/family/familyReducer.js
+++ src/redux/family/familyReducer.js
@@ -51,7 +51,24 @@ const initialState = {
   },
 };
 
-const setFamily = (state, { payload }) => ({ ...state, ...payload.info });
+const setFamily = (state, { payload }) => {
+  const {
+    balance,
+    flatPrice,
+    flatSquareMeters,
+    totalSalary,
+    passiveIncome,
+    incomePercentageToSavings,
+  } = payload.info;
+  return {
+    balance,
+    flatPrice,
+    flatSquareMeters,
+    totalSalary,
+    passiveIncome,
+    incomePercentageToSavings,
+  };
+};
 const setError = (_, { payload }) => payload;
 const unsetError = () => initialState.family.error;
 
